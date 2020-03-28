eeca3d4152b9bf9b362b8186f8a00f0ee49106eb
Sargon <sargon@me.com>
2020-03-29 00:20:11 +0100

    Fix move generator
    Generated knight moves were wrapping on borders/edges of the full bitboard

--- src/main/kotlin/name/sargon/knightpath/MoveGenerator.kt
+++ src/main/kotlin/name/sargon/knightpath/MoveGenerator.kt
@@ -1,7 +1,5 @@
 package name.sargon.knightpath
 
-import name.sargon.knightpath.NamedSquare.*
-
 
 class MoveGenerator {
     companion object {
@@ -12,26 +10,40 @@ class MoveGenerator {
         }
 
         private val knightDistances = arrayOf(
-            distanceFromE4To(F6),
-            distanceFromE4To(G5),
-            distanceFromE4To(G3),
-            distanceFromE4To(F2),
-            distanceFromE4To(D2),
-            distanceFromE4To(C3),
-            distanceFromE4To(C5),
-            distanceFromE4To(D6)
+            Pair(1, 2),
+            Pair(2, 1),
+            Pair(2, -1),
+            Pair(1, -2),
+            Pair(-1, -2),
+            Pair(-2, -1),
+            Pair(-2, 1),
+            Pair(-1, 2)
         )
 
-        private fun distanceFromE4To(to: NamedSquare): Int {
-            return to.value - E4.value
+        private fun generateMoves(board: Board, from: Square): List<Move> {
+            val moves = mutableListOf<Move>()
+
+            for (knightDistance in knightDistances) {
+                val to = squareOf(from, knightDistance)
+
+                if (to != null && board.squareIsEmpty(to)) {
+                    moves.add(Move(from, to))
+                }
+            }
+
+            return moves
         }
 
-        private fun generateMoves(board: Board, from: Square): List<Move> {
-            return knightDistances
-                .map { distance -> from + distance }
-                .filter { to -> board.squareIsEmpty(to) }
-                .map { to -> Move(from, to) }
-                .toList()
+        private fun squareOf(from: Square, knightDistance: Pair<Int, Int>): Square? {
+            var (x, y) = fromSquare(from)
+            x += knightDistance.first
+            y += knightDistance.second
+
+            if (x in 0..7 && y in 0..7) {
+                return toSquare(x, y)
+            }
+
+            return null
         }
     }
 }
